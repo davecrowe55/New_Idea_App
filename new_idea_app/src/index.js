2361e0387dcaea991c6e72d448913c6b489e632c
d4cro <33950559+davecrowe55@users.noreply.github.com>
2021-02-13 18:28:17 +1300

    CLOCK FUNCTION

--- new_idea_app/src/index.js
+++ new_idea_app/src/index.js
@@ -3,25 +3,32 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-function formatName(user) {
-  return user.firstName + ' ' + user.lastName;
+
+function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>;
 }
 
-const user = {
-  firstName: 'Harper',
-  lastName: 'Perez'
-};
-
-const elements = (
-  <h1>
-    Hello, {formatName(user)}!
-  </h1>
-);
-
+const element = <Welcome name="Sara" />;
 ReactDOM.render(
-  elements,
+  element,
   document.getElementById('root')
 );
+ 
+
+function tick() {
+  <h1>Hello, world</h1>;
+  const element = (
+    <div id>
+      <h1>Hello, world</h1>
+      <h1>Hello, world!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+  ReactDOM.render(element,  document);
+}
+
+setInterval(tick, 1000);
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
