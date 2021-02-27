730aa887aea0fb38a61e3ec4342016e6e016f504
d4cro <33950559+davecrowe55@users.noreply.github.com>
2021-02-27 16:07:51 +1300

    Added port

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ require('dotenv').config();
 const React = require('react');
 const express = require('express');
 const app = express();
+const opn = require('open'); 
+
+PORT = process.env.PORT;
 
 app.listen(PORT, () => console.log(`=== Starting your app on http://localhost:${PORT} ===`));
 opn(`http://localhost:${PORT}`);
--- new_idea_app/src/index.js
+++ new_idea_app/src/index.js
@@ -2,52 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-// import { Button } from '@material-ui/core';
-
-// function App() {
-//   return (
-//     <Button variant="contained" color="primary">
-//       Hello World
-//     </Button>
-//   );
-// }
-
-// ReactDOM.render(<App />, document.querySelector('#app'));
-
-function Welcome(props) {
-  return <h1>G'day, {props.name}</h1>;
-}
-
-const element = <Welcome name="my G" />;
-ReactDOM.render(
-  element,
-  document.getElementById('root')
-);
- 
-
-class Clock extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {date: new Date()};
-  }
-
-  render() {
-    return (
-      <div>
-        <a href="https://reactjs.org/docs/handling-events.html" onClick="console.log('The link was clicked.'); return false">
-  Click me
-</a>
-        <h1>Hello, world!</h1>
-        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
-      </div>
-    );
-  }
-}
-
-ReactDOM.render(
-  <Clock />,
-  document.getElementById('root')
-);
 
 
 
