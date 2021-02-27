9efac542a5b92ffac5ea034a198c2e81be6852b1
d4cro <33950559+davecrowe55@users.noreply.github.com>
2021-02-27 16:17:00 +1300

    npm install

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ const React = require('react');
 const express = require('express');
 const app = express();
 const opn = require('open'); 
+const header = require("./header");
+
+app.use("/header", header); 
 
 PORT = process.env.PORT;
+
 
 app.listen(PORT, () => console.log(`=== Starting your app on http://localhost:${PORT} ===`));
 opn(`http://localhost:${PORT}`);
